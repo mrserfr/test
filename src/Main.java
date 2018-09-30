eeb98bbd93371f0add4b62ef6e592a986d52c1f6
Sergey <frolov@sent.com>
2018-09-30 18:23:24 +0600

    asdas

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world! EXAMPLE EXAMPLE");
+        System.out.println("Hello, world! EXAMPLE EXAMPLE sdasdasdasdasdas");
     }
 }
 
